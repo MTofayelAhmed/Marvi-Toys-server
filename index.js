29257eaafca45fa54875bd355779a49b267eac35
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-05-19 03:44:25 +0600

    all data and dynamic data load done

--- index.js
+++ index.js
@@ -1,17 +1,81 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-
+require("dotenv").config();
 
 // middlewire
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.qhvkztn.mongodb.net/?retryWrites=true&w=majority`;
 
-app.get('/', (req, res)=>{
-  res.send('toy is running')
-})
-app.listen(port, ()=>{
-  console.log(`toys server is running on port: ${port}`)
-})
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const toyCollection = client.db("toyDB").collection("toys");
+
+    app.post("/toys", async (req, res) => {
+      const newToy = req.body;
+      console.log(newToy);
+      const result = await toyCollection.insertOne(newToy);
+      res.send(result);
+    });
+
+    app.get("/toys", async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.id;
+     
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {
+          photo: 1,
+          sellerName: 1,
+          sellerEmail: 1,
+          price: 1,
+          rating: 1,
+          quantity: 1,
+          name: 1,
+          description: 1,
+        },
+      };
+
+      const result = await toyCollection.findOne(query, options);
+
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("toy is running");
+});
+app.listen(port, () => {
+  console.log(`toys server is running on port: ${port}`);
+});
