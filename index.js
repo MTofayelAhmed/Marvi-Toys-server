b57435d884649f708ac9d45a2bdf2f4e42aea7d8
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2023-05-19 14:17:47 +0600

    toy query for individual person done

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get("/toys/:id", async (req, res) => {
-      const id = req.id;
+    app.get("/toys/:id", async (req, res)=> {
+      const id = req.params.id;
      
       const query = { _id: new ObjectId(id) };
       const options = {
@@ -60,6 +60,16 @@ async function run() {
 
       res.send(result);
     });
+
+    app.get('/toys', async(req, res)=>{
+      console.log (req.query.email)
+      let query = {}
+      if(req.query?.email){
+        query= {sellerEmail: req.query.email}
+      }
+      const result = await toyCollection.find(query).toArray()
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
